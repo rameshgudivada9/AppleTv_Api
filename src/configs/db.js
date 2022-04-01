c18f1d99c0dd80f7dc48d4406ab7f9054951ec48
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-04-01 12:54:34 +0530

    new change

--- src/configs/db.js
+++ src/configs/db.js
@@ -3,3 +3,7 @@ const mongoose = require("mongoose");
 module.exports = ()=>{
     return mongoose.connect("mongodb+srv://AppleTv:Appletv1234@appletvdata.ztlt3.mongodb.net/test");
 };
+
+// mongodb+srv://AppleTv:Appletv1234@appletvdata.ztlt3.mongodb.net/test
+
+// mongodb+srv://AppleTv:<password>@appletvdata.ztlt3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
