a0cbba507314b8953d66a67d58945b3dcd44fa86
rameshgudivada9 <96101948+rameshgudivada9@users.noreply.github.com>
2022-04-01 13:03:19 +0530

    new change

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+require("dotenv").config();
+const PORT=process.env.PORT || 6265
 const userController = require("./controllers/user.controllers");
 
 app.use("/users",userController);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require("./index.js")
 const connect = require("./configs/db")
-app.listen (6265, async function(){
+app.listen (PORT, async function(){
     await connect ();
     console.log("Listening on port 6265");
 })
